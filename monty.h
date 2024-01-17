3a0bdcaa400a6dbf7c1ee50f887ec1567619dacc
Ali Elgamal <alimelgamal199@gmail.com>
2024-01-18 00:11:39 +0200

    Modified

--- monty.h
+++ monty.h
@@ -8,10 +8,10 @@
 #include <string.h>
 #include <ctype.h>
 /**
- * struct stack_s - doubly linked list representation of stack (or queue)
- * @n: int
- * @prev: points to prev element of stack (or queue)
- * @next: points to next element of stack (or queue)
+ * struct stack_s - doubly linked list repres of stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
@@ -23,12 +23,12 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 /**
- * struct bus_s - vars -arguments, file, line content
- * @arg: value1
+ * struct bus_s - variables -args, file, line content
+ * @arg: value
  * @file: pointer to monty file
  * @content: line content
  * @lifi: flag change stack <-> queue
- * Description: carries values through program
+ * Description: carries values through the program
  */
 typedef struct bus_s
 {
@@ -40,8 +40,8 @@ typedef struct bus_s
 extern bus_t bus;
 /**
  * struct instruction_s - opcode and its function
- * @opcode: opcode
- * @f: function to handle opcode
+ * @opcode: the opcode
+ * @f: function to handle the opcode
  *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
@@ -51,11 +51,11 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-size_t getstdin(char **lineptr, int file);
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
+ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
-void f_pall(stack_t **head, unsigned int number);
 void f_push(stack_t **head, unsigned int number);
+void f_pall(stack_t **head, unsigned int number);
 void f_pint(stack_t **head, unsigned int number);
 int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
